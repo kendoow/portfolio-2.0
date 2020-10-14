dd08127c1244e02f750fc91154bd5c0268a81b87
kendoow <kendo_ow@mail.ru>
2020-10-14 23:23:53 +0300

    Update main.js

--- Js/main.js
+++ Js/main.js
@@ -4,6 +4,8 @@ $(function(){
         infinite:true,
         slidesToShow: 2,
         slidesToScroll: 2,
+        autoplay: true,
+        autoplaySpeed: 2000,
         arrows:false,
         dots:true,
     });
